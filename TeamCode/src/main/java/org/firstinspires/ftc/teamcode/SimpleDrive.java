7f4effb9310b537364808e5040459b910e6b9255
k754a <kallenms27@g.eips.ca>
2022-10-08 09:59:39 -0600

    moving with controller script

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDrive.java
@@ -0,0 +1,95 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp
+public class SimpleDrive extends OpMode {
+
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    @Override
+    public void init()
+    {
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back");
+
+
+
+        leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+
+    @Override
+    public void loop() {
+        double leftFrontPower;
+        double leftBackPower;
+        double rightFrontPower;
+        double rightBackPower;
+
+
+        double drive = -gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x;
+
+        leftFrontPower = Range.clip(drive+ turn + strafe, -1, 1);
+        rightFrontPower = Range.clip(drive - turn - strafe, -1, 1);
+        leftBackPower = Range.clip(drive + turn - strafe, -1, 1);
+        rightBackPower = Range.clip(drive - turn + strafe, -1, 1);
+
+
+        if(gamepad1.right_stick_x >= 0.1 && gamepad1.left_stick_y <= -0.1){
+            rightFrontPower = 0.5;
+            rightBackPower = 0.5;
+            leftFrontPower = -0.5;
+            leftBackPower = -0.5;
+        }
+
+        else if(gamepad1.right_stick_x <= -0.1 && gamepad1.left_stick_y <= -0.1){
+            leftFrontPower = 0.5;
+            leftBackPower = 0.5;
+            rightFrontPower = -0.5;
+            rightBackPower = -0.5;
+        }
+
+        leftFrontDrive.setPower((leftFrontPower));
+        rightFrontDrive.setPower((rightFrontPower));
+        leftBackDrive.setPower((leftBackPower));
+        rightBackDrive.setPower((rightBackPower));
+
+        //using for button
+//        if (gamepad2.a)
+//        {
+//            motor.setpower(1);
+//        }
+
+}
+
+    @Override
+    public void stop()
+    {
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+    }
+
+    }
+
