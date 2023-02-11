72a15d681b036d12fcfcaae87e8bf00235a5a639
AaronTheGoat <aaronz27@g.eips.ca>
2023-02-10 21:58:19 -0700

    updated the potential auto MeepMeepTesting

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -7,27 +7,18 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-35, -61, 7.86))
-                                .strafeLeft(20)
-                                .strafeLeft(-20)
-                                .forward(50)
-                                .turn(Math.toRadians(90))
-                                .waitSeconds(1)
-                                .forward(20)
-                                .waitSeconds(1)
-                                .back(20)
-                                .waitSeconds(1.5)
-                                .turn(Math.toRadians(45))
-                                .waitSeconds(2)
+                                .forward(25)
                                 .turn(Math.toRadians(-45))
+                                .waitSeconds(1)
+                                .turn(Math.toRadians(45))
                                 .strafeLeft(25)
-                                .back(25)
                                 .build()
                 );
 
